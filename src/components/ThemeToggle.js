1f40f875d55c47f9735fc106498d7b3365f63a9b
jhlee <makk7201@gmail.com>
2023-10-21 01:12:04 +0900

    Modify <이준형 : ThemeToggle.js>
    - prettier 포맷터 적용

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,6 +1,6 @@
+import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { changeTheme } from "../store/themeSlice.js";
-import styled from "styled-components";
 
 function ThemeToggle() {
   let dispatch = useDispatch();
@@ -10,13 +10,15 @@ function ThemeToggle() {
       onClick={() => {
         dispatch(changeTheme());
       }}
-    >테마 변경</StyledBtn>
+    >
+      테마 변경
+    </StyledBtn>
   );
 }
 
 let StyledBtn = styled.button`
-    width: 100px;
-    height: 32px;
-`
+  width: 100px;
+  height: 32px;
+`;
 
 export default ThemeToggle;
