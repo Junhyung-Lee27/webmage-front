c8ff2fa7a176ed195ecae60f7482fa2466e5f2a4
jhlee <makk7201@gmail.com>
2023-10-20 21:40:39 +0900

    Update <이준형 : App.js>
    - react-router 설정 추가
    - 테마 설정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Auth/LoginPage";
+import { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
 
 function App() {
+  const currentTheme = useSelector((state) => state.theme.themes[state.theme.currentTheme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={currentTheme}>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
