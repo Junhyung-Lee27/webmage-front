3ce7c82c66042100684d252ceb72675fcbb5d2ca
jhlee <makk7201@gmail.com>
2023-10-21 01:13:40 +0900

    Add <이준형 : theme colors>
    - 컬러 테마에 background2, bordercolor 추가

--- src/store/themeSlice.js
+++ src/store/themeSlice.js
@@ -5,13 +5,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const themes = {
   light: {
     backgroundColor: "#FFFFFF",
+    backgroundColor2: "#EBE9FA",
     primaryColor: "#7269FF",
     secondaryColor: "#251F4B",
+    borderColor: "#D8D6E2",
   },
   dark: {
     backgroundColor: "#0F0B21",
+    backgroundColor2: "#221C44",
     primaryColor: "#FFFFFF",
     secondaryColor: "#7269FF",
+    borderColor: "#3B336C",
   },
 };
 
