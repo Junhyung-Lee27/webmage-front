f9850aa473c0b93a5972ddddc2b055fc56b320cf
Junhyung-Lee <makk7201@gmail.com>
2023-10-22 18:36:54 +0900

    Add <이준형 : 소셜 로그인 버튼>
    - 구글, 네이버, 카카오 로그인 버튼 추가
    - 로고는 아직 넣지 못함

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,10 +6,10 @@ function LoginForm() {
 
   return (
     <Column>
-      <StyledText color={currentTheme.secondaryColor} size="32" weight="700">
+      <StyledText color={currentTheme.font1} size="32" weight="700">
         웹법사와 함께 만드는
       </StyledText>
-      <StyledText color={currentTheme.primaryColor} size="32" weight="700" align="right">
+      <StyledText color={currentTheme.primary} size="32" weight="700" align="right">
         만다라트
       </StyledText>
       <FormGroup></FormGroup>
@@ -22,29 +22,61 @@ function FormGroup(props) {
 
   return (
     <div>
-      <Column gap="8">
-        <StyledText color={currentTheme.secondaryColor} size="14" weight="600">
+      <Column>
+        <StyledText color={currentTheme.font1} size="14" weight="600" margin="32px 0px 0px 0px">
           <label htmlFor="user-id">아이디</label>
         </StyledText>
         <StyledForm
           type="email"
           placeholder="이메일을 입력해주세요"
           id="user-id"
-          placeholderColor={currentTheme.secondaryColor}
-          backgroundColor={currentTheme.backgroundColor}
-          borderColor={currentTheme.primaryColor}
+          fontColor={currentTheme.font1}
+          placeholderColor={currentTheme.font2}
+          backgroundColor={currentTheme.bg2}
+          borderColor={currentTheme.font1}
+          margin="4px 0px 0px 0px"
         ></StyledForm>
-        <StyledText color={currentTheme.secondaryColor} size="14" weight="600">
+        <StyledText color={currentTheme.font1} size="14" weight="600" margin="16px 0px 0px 0px">
           <label htmlFor="password">비밀번호</label>
         </StyledText>
         <StyledForm
           type="password"
           placeholder="비밀번호를 입력해주세요"
           id="password"
-          placeholderColor={currentTheme.secondaryColor}
-          backgroundColor={currentTheme.backgroundColor}
-          borderColor={currentTheme.primaryColor}
+          fontColor={currentTheme.font1}
+          placeholderColor={currentTheme.font2}
+          backgroundColor={currentTheme.bg2}
+          borderColor={currentTheme.font1}
+          margin="4px 0px 24px 0px"
         ></StyledForm>
+        <StyledButton 
+          color= "white"
+          backgroundColor={currentTheme.primary}
+          borderColor={currentTheme.primary}
+          margin="0px 2px">
+          로그인
+        </StyledButton>
+        <StyledButton 
+          color= "black"
+          backgroundColor= "#FFFFFF"
+          borderColor= "#000000"
+          margin="8px 2px 0px 2px">
+          구글로 시작하기
+        </StyledButton>
+        <StyledButton 
+          color= "#191919"
+          backgroundColor= "#FEE500"
+          borderColor= "#FEE500"
+          margin="8px 2px 0px 2px">
+          카카오로 시작하기
+        </StyledButton>
+        <StyledButton
+          color= "white"
+          backgroundColor= "#03C75A"
+          borderColor= "#03C75A"
+          margin="8px 2px 0px 2px">
+          네이버로 시작하기
+        </StyledButton>
       </Column>
     </div>
   );
@@ -53,7 +85,6 @@ function FormGroup(props) {
 let Column = styled.div`
   display: flex;
   flex-direction: column;
-  gap: ${({ gap }) => gap + "px"};
 `;
 
 let StyledText = styled.span`
@@ -61,21 +92,39 @@ let StyledText = styled.span`
   font-weight: ${({ weight }) => weight};
   color: ${({ color }) => color};
   text-align: ${({ align }) => align};
+  margin: ${({margin}) => margin};
 `;
 
 let StyledForm = styled.input`
   height: 24px;
   padding: 8px 16px;
   font-size: 14px;
+  color: ${({ fontColor }) => fontColor};
+  border: none;
+  border-radius: 8px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  margin: ${({margin}) => margin};
+  &::placeholder {
+    color: ${({ placeholderColor }) => placeholderColor};
+    opacity: 0.5;
+  }
+  &:focus {
+    outline: 2px solid ${({borderColor}) => borderColor};
+  }
+`;
+
+let StyledButton = styled.button`
+  height: 42px;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 20px;
+  margin: ${({margin}) => margin};
+  color: ${({ color }) => color};
   background-color: ${({ backgroundColor }) => backgroundColor};
   border: 1px solid ${({ borderColor }) => borderColor};
   border-radius: 8px;
   outline: none;
+`
 
-  &::placeholder {
-    color: ${({ placeholderColor }) => placeholderColor};
-    opacity: 0.7;
-  }
-`;
 
 export default LoginForm;
