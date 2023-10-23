11bbe9baa49523d45c039223bd0322ea75850fc2
jhlee <makk7201@gmail.com>
2023-10-23 17:59:22 +0900

    Init <이준형 : SettingPage>

---
+++ src/pages/SettingPage.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+function SettingPage() {
+  return (
+    <div>
+      <Header></Header>
+      <div>환경설정 페이지입니다.</div>
+    </div>
+  );
+}
+
+export default SettingPage;
