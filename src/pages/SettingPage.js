6bf031272060abf87a14c2919825710486217af4
Junhyung-Lee <makk7201@gmail.com>
2023-10-23 21:54:47 +0900

    Modify <이준형 : SettingPage>

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -1,12 +1,18 @@
+import styled from "styled-components";
 import Header from "../components/Header";
 
 function SettingPage() {
   return (
-    <div>
+    <Column>
       <Header></Header>
       <div>환경설정 페이지입니다.</div>
-    </div>
+    </Column>
   );
 }
+
+let Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default SettingPage;
