3e31d69886f327addf8ba2f0e52164dc8520206d
jhlee <makk7201@gmail.com>
2023-10-24 13:15:53 +0900

    Modify <이준형 : ThemeSwitch.js>
    - 테마 변경시 보여지는 이미지 변경

--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -1,42 +1,29 @@
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { changeTheme } from "../store/themeSlice.js";
+import DarkUrl from "./../assets/images/Dark.svg";
+import LightUrl from "./../assets/images/Light.svg";
 
 function ThemeSwitch() {
   let dispatch = useDispatch();
-  const currentTheme = useSelector((state) => state.theme.themes[state.theme.currentTheme]);
+  const currentTheme = useSelector((state) => state.theme.currentTheme);
 
   return (
     <SwitchContainer
-      backgroundColor={currentTheme.bg2}
       onClick={() => {
         dispatch(changeTheme());
       }}
-    >
-      <ThemeIcon active={currentTheme === "dark"} />
-    </SwitchContainer>
+      theme={currentTheme}
+    ></SwitchContainer>
   );
 }
 
-let SwitchContainer = styled.button`
-  width: 96px;
+let SwitchContainer = styled.div`
+  width: 88px;
   height: 36px;
   border-radius: 18px;
-  background-color: ${({ backgroundColor }) => backgroundColor};
-  position: relative;
-`;
-
-const ThemeIcon = styled.div`
-  width: 32px; 
-  height: 32px;
-  border-radius: 20px;
-  background-image: url(${props => props.theme === 'light' ? './../assets/images/sun-icon.svg' : './../assets/images/moon-icon.svg'});  background-size: cover;
   background-size: cover;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  left: ${props => props.active ? 'calc(100% - 40px - 5px)' : '5px'}; // 5px는 여백입니다
-  transition: left 0.3s ease-in-out;
+  background-image: url(${(props) => (props.theme === "light" ? LightUrl : DarkUrl)});
 `;
 
 export default ThemeSwitch;
