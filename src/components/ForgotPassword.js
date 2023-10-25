b5e0e8ab19d122ca694e9211a7f79874824870e8
jhlee <makk7201@gmail.com>
2023-10-25 21:11:57 +0900

    Revert "Revert "Add <이준형 : profile image edit icon>""
    This reverts commit 3e9882a6963a53bc34e1ec8b0e192953f54f6d5e.
    
    revert

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -64,7 +64,7 @@ let StyledForm = styled.input`
     opacity: 0.5;
   }
   &:focus {
-    outline: 2px solid ${({ theme }) => theme.border};
+    outline: 2px solid ${({ theme }) => theme.primary};
   }
 `;
 
