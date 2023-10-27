c0d6e26449c3f2a8120b7ffd01387816211974a6
jhlee <makk7201@gmail.com>
2023-10-27 10:10:01 +0900

    Modify <이준형: 차단 리스트 테이블 구조 오류 수정>

--- src/components/BlockedUsers.js
+++ src/components/BlockedUsers.js
@@ -11,25 +11,31 @@ function BlockedUsers() {
 
   // 예시 데이터
   const users = [
-    { username: '차단유저1', date: '2023.10.25' },
-    { username: '차단유저2', date: '2023.10.24' },
-    { username: '차단유저3', date: '2023.10.23' }
+    { username: "차단유저1", date: "2023.10.25" },
+    { username: "차단유저2", date: "2023.10.24" },
+    { username: "차단유저3", date: "2023.10.23" },
   ];
 
   return (
     <ThemeProvider theme={combinedTheme}>
       <BlockedTable>
         <TableHead>
-          <TableHeaderCell>닉네임</TableHeaderCell>
-          <TableHeaderCell>차단 일시</TableHeaderCell>
-          <TableHeaderCell></TableHeaderCell>
+          <tr>
+            <TableHeaderCell>닉네임</TableHeaderCell>
+            <TableHeaderCell>차단 일시</TableHeaderCell>
+            <TableHeaderCell></TableHeaderCell>
+          </tr>
         </TableHead>
-        {users.map(user => (
-          <UserContainer key={user.username + user.date}>
-            <Username>{user.username}</Username>
-            <BlockedDate>{user.date}</BlockedDate>
-            <UnblockBtnBox><UnblockBtn>차단 해제</UnblockBtn></UnblockBtnBox>
-          </UserContainer>
+        {users.map((user) => (
+          <tbody>
+            <UserContainer key={user.username + user.date}>
+              <Username>{user.username}</Username>
+              <BlockedDate>{user.date}</BlockedDate>
+              <UnblockBtnBox>
+                <UnblockBtn>차단 해제</UnblockBtn>
+              </UnblockBtnBox>
+            </UserContainer>
+          </tbody>
         ))}
       </BlockedTable>
     </ThemeProvider>
@@ -41,7 +47,7 @@ let BlockedTable = styled.table`
   flex-grow: 1;
   margin-left: auto;
   margin-right: auto;
-`
+`;
 
 let TableHead = styled.thead`
   height: 56px;
@@ -58,9 +64,14 @@ let CenteredText = styled.td`
   cursor: default;
 `;
 
-let TableHeaderCell  = styled(CenteredText)`
+let TableHeaderCell = styled.th`
+  width: 33%;
+  font-size: 14px;
+  text-align: center;
+  line-height: 56px;
   font-weight: 600;
   color: ${({ theme }) => theme.color.font1};
+  cursor: default;
 `;
 
 let Username = styled(CenteredText)`
@@ -75,7 +86,7 @@ let UserContainer = styled.tr`
   height: 56px;
   width: 100%;
   border-bottom: 1px solid ${({ theme }) => theme.color.border};
-`
+`;
 
 let UnblockBtn = styled.button`
   width: 40%;
@@ -92,8 +103,8 @@ let UnblockBtn = styled.button`
   cursor: pointer;
 
   &:hover {
-  color: ${({ theme }) => theme.color.bg};
-  background-color: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.bg};
+    background-color: ${({ theme }) => theme.color.primary};
   }
 `;
 
@@ -101,6 +112,6 @@ let UnblockBtnBox = styled.td`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export default BlockedUsers;
