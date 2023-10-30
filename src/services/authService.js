51726e5bad61c10ecb8cabcb973312ab7420752b
jhlee <makk7201@gmail.com>
2023-10-30 17:25:47 +0900

    Modify <이준형 : 토큰 가져오기 함수 수정>

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,7 +5,7 @@ export const getToken = async () => {
     const response = await axios.get("http://15.164.217.203:8000/get_token/");
 
     if (response.status === 200) {
-      return { success: true, csrfToken: response.data.csrf_token };
+      return { success: true, token: response.data.csrf_token };
     } else {
       return { error: "나중에 다시 시도해주세요." };
     }
@@ -51,6 +51,8 @@ export const login = async (username, password) => {
       password,
     });
 
+    console.log(response);
+
     // 응답 처리
     if (response.status === 200 && response.statusText === "OK") {
       return { success: true };
@@ -84,30 +86,50 @@ export const editAccount = async (username, email, password, passwordCheck) => {
   // 비밀번호 불일치할 경우
   if (password !== passwordCheck) {
     return { error: "비밀번호가 일치하지 않습니다." };
-  }
-
-  try {
-    const response = await axios.patch("http://15.164.217.203:8000/user/edit/", {
+  } else {
+    const requestData = {
       username,
       email,
       password,
-    });
+    };
 
-    if (response.status === 200 && response.statusText === "OK") {
-      return { success: true };
-    } else {
-      return { error: "프로필 수정에 실패했습니다. 나중에 다시 시도해주세요" };
+    const axiosConfig = {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      withCredentials: true
+    };
+
+    try {
+      const response = await axios.patch(
+        "http://15.164.217.203:8000/user/edit/",
+        requestData,
+        axiosConfig
+      );
+
+      if (response.status === 200 && response.statusText === "OK") {
+        return { success: true };
+      } else {
+        return { error: "프로필 수정에 실패했습니다. 나중에 다시 시도해주세요" };
+      }
+    } catch (error) {
+      console.error("userEdit Api Error:", error);
+      return { error: " 프로필 수정 중 오류가 발생했습니다. 나중에 다시 시도해주세요." };
     }
-  } catch (error) {
-    console.error("userEdit Api Error:", error);
-    return { error: " 프로필 수정 중 오류가 발생했습니다. 나중에 다시 시도해주세요." };
   }
 };
 
 // 회원탈퇴
-export const deleteUser = async () => {
+export const deleteUser = async (token) => {
   try {
-    const response = await axios.delete("http://15.164.217.203:8000/user/delete-user/");
+    const response = await axios.delete("http://15.164.217.203:8000/user/delete-user/", {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        "X-CSRFToken": token,
+      },
+    });
 
     if (response.status === 200 && response.statusText === "OK") {
       return { success: true };
