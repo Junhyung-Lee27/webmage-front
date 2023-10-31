ef766dce72950831402fad02fec94aca625e6343
jhlee <makk7201@gmail.com>
2023-10-31 18:14:47 +0900

    Update <이준형 : token Authentication 방식으로 변경>

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,4 @@
 import axios from "axios";
-// 토큰 가져오기
-export const getToken = async () => {
-  try {
-    const response = await axios.get("http://15.164.217.203:8000/get_token/");
-
-    if (response.status === 200) {
-      return { success: true, token: response.data.csrf_token };
-    } else {
-      return { error: "나중에 다시 시도해주세요." };
-    }
-  } catch (error) {
-    console.error("getToken API error:", error);
-    return { error: "나중에 다시 시도해주세요." };
-  }
-};
 
 // 회원가입
 export const signup = async (username, email, password, passwordCheck) => {
@@ -24,7 +9,7 @@ export const signup = async (username, email, password, passwordCheck) => {
 
   // 비밀번호 일치할 경우
   try {
-    const response = await axios.post("http://15.164.217.203:8000/user/signup/", {
+    const response = await axios.post("http://127.0.0.1:8000/user/signup/", {
       username,
       email,
       password,
@@ -32,7 +17,7 @@ export const signup = async (username, email, password, passwordCheck) => {
 
     // 응답 처리
     if (response.status === 201) {
-      return { success: true };
+      return { success: true, token: response.data.token };
     } else {
       return { error: "회원가입에 실패했습니다. 아이디, 이메일, 비밀번호 형식을 확인해주세요." };
     }
@@ -46,14 +31,14 @@ export const signup = async (username, email, password, passwordCheck) => {
 // 로그인
 export const login = async (username, password) => {
   try {
-    const response = await axios.post("http://15.164.217.203:8000/user/login/", {
+    const response = await axios.post("http://127.0.0.1:8000/user/login/", {
       username,
       password,
     });
 
     // 응답 처리
-    if (response.status === 200 && response.statusText === "OK") {
-      return { success: true };
+    if (response.status === 200) {
+        return { success: true, token: response.data.token };
     } else {
       return { error: "로그인에 실패했습니다. 아이디나 비밀번호를 확인해주세요." };
     }
@@ -66,9 +51,9 @@ export const login = async (username, password) => {
 // 로그아웃
 export const logout = async () => {
   try {
-    const response = await axios.post("http://15.164.217.203:8000/user/logout/");
+    const response = await axios.post("http://127.0.0.1:8000/user/logout/");
 
-    if (response.status === 200 && response.statusText === "OK") {
+    if (response.status === 200) {
       return { success: true };
     } else {
       return { error: "로그아웃에 실패했습니다. 나중에 다시 시도해주세요." };
@@ -80,7 +65,7 @@ export const logout = async () => {
 };
 
 // 프로필 수정
-export const editAccount = async (username, email, password, passwordCheck) => {
+export const editAccount = async (username, email, password, passwordCheck, token) => {
   // 비밀번호 불일치할 경우
   if (password !== passwordCheck) {
     return { error: "비밀번호가 일치하지 않습니다." };
@@ -95,18 +80,18 @@ export const editAccount = async (username, email, password, passwordCheck) => {
       headers: {
         accept: "application/json",
         "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
       },
-      withCredentials: true,
     };
 
     try {
       const response = await axios.patch(
-        "http://15.164.217.203:8000/user/edit/",
+        "http://127.0.0.1:8000/user/edit/",
         requestData,
         axiosConfig
       );
 
-      if (response.status === 200 && response.statusText === "OK") {
+      if (response.status === 200) {
         return { success: true };
       } else {
         return { error: "프로필 수정에 실패했습니다. 나중에 다시 시도해주세요" };
@@ -119,17 +104,19 @@ export const editAccount = async (username, email, password, passwordCheck) => {
 };
 
 // 회원탈퇴
-export const deleteUser = async () => {
+export const deleteUser = async (user, token) => {
   try {
-    const response = await axios.delete("http://15.164.217.203:8000/user/delete-user/", {
+    const response = await axios.delete("http://127.0.0.1:8000/user/delete-user/", {
       headers: {
         accept: "application/json",
-        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
       },
-      withCredentials: true,
+      data: {
+        user: user,
+      },
     });
 
-    if (response.status === 200 && response.statusText === "OK") {
+    if (response.status === 200) {
       return { success: true };
     } else {
       return { error: "회원탈퇴에 실패했습니다. 나중에 다시 시도해주세요." };
