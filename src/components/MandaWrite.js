9a1f68b56af2cdce53a4f946e0e09e361bb51350
Yumi Heo <hymsimple@gmail.com>
2023-11-03 04:42:22 +0900

    Modify <허유미 : contents 임시데이터 넣고 반쯤 성공>

--- src/components/MandaWrite.js
+++ src/components/MandaWrite.js
@@ -1,273 +1,256 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import { useSelector } from "react-redux";
-import MandaModal from "./MandaModal";
-
-// 모달에 입력한 데이터를 가져오는 함수
-// 캐시를 저장할 객체 생성
-const dataCache = {};
-
-async function fetchMandaData(mainId) {
-  // 이미 캐시된 데이터가 있는지 확인
-  if (dataCache[mainId]) {
-    return dataCache[mainId];
-  }
-
-  try {
-    const response = await axios.get(`http://127.0.0.1:8000/manda/mandasimple/${mainId}`);
-    const data = response.data;
-
-    // 데이터를 캐시에 저장
-    dataCache[mainId] = data;
-
-    return data;
-  } catch (error) {
-    console.error("데이터 가져오기 오류:", error);
-    return null;
-  }
-}
 
 
 function MandaWrite() {
   const currentTheme = useSelector((state) => state.theme.themes[state.theme.currentTheme]);
 
   const user = useSelector((state) => state.user);
+  const authToken = useSelector((state) => state.user.authToken);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [mainId, setMainId] = useState(null);
-  const [data, setData] = useState(null); // 데이터 상태 추가
+  const [mainTitle, setMainTitle] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-  const [tableData, setTableData] = useState([
-    [[[["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]]],
-    [[["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]]],
-    [[["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]]]],
-  ]);
+  const initialInputValues = Array(8).fill().map(() => Array(9).fill(''));
+  const [inputValues, setInputValues] = useState(initialInputValues);
 
-  const handleCellChange = (tableIndex, rowIndex, cellIndex, value) => {
-    const updatedTableData = [...tableData];
-    updatedTableData[tableIndex][rowIndex][cellIndex] = value;
-    setTableData(updatedTableData);
-  };
+  const [subsId, setSubsId] = useState([]);
+  const [contentsId, setContentsId] = useState([]);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleModalClose = (mainId) => {
-    setIsModalOpen(false);
-    setMainId(mainId); // main_id를 업데이트
-  };
-
-  useEffect(() => {
-    if (handleModalClose && mainId) {
-      // 모달이 열릴 때와 mainId가 변경될 때 데이터 가져오기
-      fetchMandaData(mainId)
-        .then((result) => {
-          setData(result);
-        });
-    }
-  }, [handleModalClose, mainId]);
-
-    // Save 버튼을 눌렀을 때 호출되는 함수
-    const saveData = async () => {
-      try {
-        const requestData = {
-          contents: [],
-        };
+  async function handleAddButtonClick() {
+    setIsButtonDisabled(true); // 버튼 비활성화
   
-        // 테이블에 있는 데이터를 서버 요청에 맞게 변환하고 requestData에 추가
-        Array.from({ length: 9 }, (_, tableIndex) => {
-          Array.from({ length: 3 }, (_, rowIndex) => {
-            Array.from({ length: 3 }, (_, cellIndex) => {
-              const adjustedCellIndex = rowIndex * 3 + cellIndex;
-              if (
-                // 제외 조건 추가: tableIndex가 4가 아니며 rowIndex와 cellIndex가 1인 경우를 제외
-                !(tableIndex !== 4 && rowIndex === 1 && cellIndex === 1)
-              ) {
-                const id = (mainId - 1) * 64 + adjustedCellIndex + 1;
-                const content = tableData[rowIndex][cellIndex];
-                const success_count = 0;
-                requestData.contents.push({ id, content, success_count });
-              }
-            });
-          });
-        });
-  
-        // 서버로 데이터 전송
-        const response = await axios.post('http://127.0.0.1:8000/manda/edit/content/', requestData);
-        console.log('Data saved:', response.data);
-  
-        // 필요한 후속 작업 수행
-      } catch (error) {
-        console.error('오류 발생:', error);
-        // 오류 처리 코드
-      }
+    const requestDataCreate = {
+      user: user.userId,
+      main_title: mainTitle,
+      success: false,
     };
-    
+  
+    try {
+      const createResponse = await axios.post('http://127.0.0.1:8000/manda/create/', requestDataCreate, {
+        headers: {
+          Authorization: `Token ${authToken}`,
+        },
+      });
+      // 요청이 성공하면 원하는 작업을 수행
+      console.log('POST 요청 성공', createResponse.data);
+      const mainId = createResponse.data.main.id;
+      console.log(mainId);
+
+      // POST 요청 이후 GET 요청 보내기
+      try {
+        const getResponse = await axios.get(`http://127.0.0.1:8000/manda/mandamain/${mainId}`);
+        // GET 요청의 응답 데이터를 추출하고 필요한 부분을 가공할 수 있습니다.
+        const data = getResponse.data;
+
+        const newSubsId = data.subs.map((sub) => sub.id);
+        const newContentsId = data.contents.map((content) => content.id);
+
+        setSubsId(newSubsId);
+        setContentsId(newContentsId);
+
+        console.log('GET 요청 성공', subsId, contentsId);
+      } catch (error) {
+        // GET 요청이 실패하면 오류 처리
+        console.error('GET 요청 실패', error);
+      }
+
+
+    } catch (error) {
+      // 요청이 실패하면 오류 처리
+      console.error('POST 요청 실패', error);
+    } finally {
+      setIsButtonDisabled(false); // 요청 완료 후 여전히 버튼 비활성화
+    }
+  }
+  
+
+
+  async function handleSaveBtnClick() {
+    const contentData = contentsId.map((id) => {
+      return {
+        id: id,
+        content: '임시용',
+        success_count: 0
+      };
+    });
+  
+    const contentsRequestData = {
+      contents: contentData,
+    };
+  
+    try {
+      const contentsResponse = await axios.post('http://127.0.0.1:8000/manda/edit/content/', contentsRequestData, {
+        headers: {
+          Authorization: `Token ${authToken}`,
+        },
+      });
+      // Process the contents response as needed
+      console.log('contents POST 요청 성공', contentsResponse.data);
+    } catch (error) {
+      // Handle errors for the contents POST request
+      console.error('contents POST 요청 실패', error);
+    }
+  
+    const subsData = subsId.map((id, listIndex) => {
+      return {
+        id: id,
+        sub_title: inputValues[listIndex][0],
+        success: true, // success 필드에는 원하는 값 사용
+      };
+    });
+  
+    const requestData = {
+      subs: subsData,
+    };
+  
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/manda/edit/sub/', requestData, {
+        headers: {
+          Authorization: `Token ${authToken}`,
+        },
+      });
+      // Process the subs response as needed
+      console.log('subs POST 요청 성공', response.data);
+    } catch (error) {
+      // Handle errors for the subs POST request
+      console.error('subs POST 요청 실패', error);
+    }
+  }
+  
+
+  function handleMainInputChange(event) {
+    setMainTitle(event.target.value);
+  }
+
+  function handleInputChange(e, listIndex, index) {
+    const newValue = e.target.value;
+    setInputValues((prevInputValues) => {
+      const newInputValues = [...prevInputValues];
+      newInputValues[listIndex][index] = newValue;
+      return newInputValues;
+    });
+  }
+
 
   return (
     <div>
-      <Manda>
-        <div>
-          <MandaModal isOpen={isModalOpen} onClose={handleModalClose} />
-          <GridContainer>
-            {Array.from({ length: 9 }, (_, tableIndex) => (
-              <GridItem key={tableIndex}>
-                <tbody>
-                  {Array.from({ length: 3 }, (_, rowIndex) => (
-                    <tr key={rowIndex}>
-                      {Array.from({ length: 3 }, (_, cellIndex) => (
-                        <td key={cellIndex}>
-                          <TableCell
-                            type="text"
-                            maxLength="8"
-                            onChange={(e) => handleCellChange(tableIndex, rowIndex, cellIndex, e.target.value)}
-                            value={
-                              tableIndex === 4
-                                ? rowIndex === 1 && cellIndex === 1
-                                  ? data?.main_title || ''
-                                  : rowIndex === 0
-                                  ? data?.subs[rowIndex * 3 + cellIndex]?.sub_title || ''
-                                  : rowIndex === 1 && cellIndex === 0
-                                  ? data?.subs[rowIndex * 3 + cellIndex]?.sub_title || ''
-                                  : data?.subs[rowIndex * 3 + cellIndex - 1]?.sub_title || ''
-                                : tableIndex >= 0 && tableIndex <= 3
-                                  ? rowIndex === 1 && cellIndex === 1
-                                    ? data?.subs[tableIndex]?.sub_title || ''
-                                    : ''
-                                  : tableIndex >= 5 && tableIndex <= 8
-                                    ? rowIndex === 1 && cellIndex === 1
-                                      ? data?.subs[tableIndex - 1]?.sub_title || ''
-                                      : ''
-                                    : ''
-                            }
-                            disabled={
-                              !(
-                                (tableIndex !== 4 && !(rowIndex === 1 && cellIndex === 1)) 
-                              )
-                            }
+      <ContentsContainer>
+        <MainAdd>
+          <MainInput
+              type="text"
+              placeholder="main_title"
+              value={mainTitle}
+              onChange={handleMainInputChange}
+            />
+          <AddTitleButton onClick={handleAddButtonClick} disabled={isButtonDisabled}>핵심 목표 등록</AddTitleButton>
+          <SaveBtn onClick={handleSaveBtnClick}>만다라트 저장</SaveBtn>
+        </MainAdd>
 
-                          />
-                        </td>
-                      ))}
-                    </tr>
+        <SubGroup>
+          {Array(8)
+            .fill(0)
+            .map((_, listIndex) => (
+              <StyledList key={listIndex}>
+                {Array(9)
+                  .fill(0)
+                  .map((_, index) => (
+                    <StyledListItem key={index}>
+                      <StyledInput
+                        type="text"
+                        placeholder={index === 0 ? `세부목표 ${listIndex+1}` : `실천 계획 ${index}`}
+                        value={inputValues[listIndex][index]} // 값을 표시하고 사용자 입력을 추적합니다.
+                        onChange={(e) => handleInputChange(e, listIndex, index)} // 입력 값이 변경될 때 호출됩니다.
+                      />
+                    </StyledListItem>
                   ))}
-                </tbody>
-              </GridItem>
+              </StyledList>
             ))}
-          </GridContainer>
-        </div>
-
-        <BtnGroup>
-          <StartBtn onClick={openModal}>시작</StartBtn>
-          <SaveBtn onClick={saveData}>저장</SaveBtn>
-          <DeleteBtn>삭제</DeleteBtn>
-        </BtnGroup>
-      </Manda>
+        </SubGroup>
+      </ContentsContainer>
     </div>
   );
-}
+};
 
-const Manda = styled.div`
+
+const ContentsContainer = styled.div`
   display: flex;
-  gap: 20px;
-  position: relative;
-  flex-shrink: 0;
-`;
-
-const GridContainer = styled.div`
-  display: inline-flex;
   flex-wrap: wrap;
-  width: 920px;
+  width: 1000px;
   height: 646px;
   margin-left: 198px;
   margin-top: -15px;
-  border-radius: 8px;
-  box-shadow: 0px 8px 24px 0px rgba(0, 0, 0, 0.15);
   flex-shrink: 0;
+  gap: 15px;
 `;
 
-const GridItem = styled.table`
-  margin: 0;
-  flex: 0 0 calc(33.3333% - 0px); /* 3개씩 가로로 배치, 0px는 border 두께 */
-  box-sizing: border-box;
-  table-layout: fixed;
-  border-collapse: separate;
-  border-spacing: 1px;
-  border: 2px solid rgba(0, 0, 0, 0.15);
-  flex-shrink: 0;
-`;
-
-const TableCell = styled.input`
-  text-align: center;
-  vertical-align: middle;
-  width: 100px;
-  height: 70px;
-  word-break: break-word;
-  white-space: normal;
-  padding: 0.5px;
-  border: 1px solid #fff;
-  background: rgba(114, 105, 255, 0.1);
-  font-size: 12px;
-`;
-
-const BtnGroup = styled.div`
+const MainAdd = styled.div`
   display: flex;
-  gap: 10px;
-  border: none;
-  flex-direction: column;
+  // justify-contents: center;
+`
+
+const MainInput = styled.input`
+  display: flex;
+  width: 200px;
+  height: 30px;
+  border: 1px solid #ddd;
+  margin-right: 10px;
 `;
 
-const StartBtn = styled.button`
-  width: 200px;
-  height: 42px;
-  border-radius: 4px;
+const AddTitleButton = styled.button`
+  width: 120px;
+  height: 30px;
   border: none;
   background: #6c63ff;
-  flex-shrink: 0;
   color: #fff;
-  justify-content: center;
-  font-size: 20px;
-  font-weight: 700;
+  font-size: 16px;
+  font-weight: 600;
   cursor: pointer;
-  margin-left: 10px;
+  display: flex;
+  align-items: center;
+  margin-right: 100px;
 `;
-
 
 const SaveBtn = styled.button`
-  width: 200px;
-  height: 42px;
-  border-radius: 4px;
+  width: 120px;
+  height: 30px;
   border: none;
-  background: #6c63ff;
-  flex-shrink: 0;
+  background: green;
   color: #fff;
-  justify-content: center;
-  font-size: 20px;
-  font-weight: 700;
+  font-size: 16px;
+  font-weight: 600;
   cursor: pointer;
-  margin-left: 10px;
+  display: flex;
+  align-items: center;
+`
+
+const SubGroup = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`
+const StyledList = styled.ol`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 230px;
+  height: 300px;
 `;
 
-const DeleteBtn = styled.button`
+const StyledListItem = styled.li`
   width: 200px;
-  height: 42px;
-  border-radius: 4px;
-  border: none;
-  background: #c31616;
-  flex-shrink: 0;
-  color: #fff;
-  justify-content: center;
-  font-size: 20px;
-  font-weight: 700;
-  cursor: pointer;
-  margin-left: 10px;
+  height: 30px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  height: 100%;
+  border: none; 
+  border-bottom: 1px solid #ddd;
 `;
 
 export default MandaWrite;
