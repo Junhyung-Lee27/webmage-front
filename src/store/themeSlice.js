015eeacf79412b25e81a069c03e65df2bdbde9a0
jhlee <makk7201@gmail.com>
2024-01-15 23:08:44 +0900

    Modify <이준형 : 테마 컬러 수정>

--- src/store/themeSlice.js
+++ src/store/themeSlice.js
@@ -7,8 +7,8 @@ const themes = {
     bg: "#FFFFFF", // 가장 밝은 배경
     bg2: "#FDFDFD", // 덜 밝은 배경
     bg3: "#EDEDED", // 더 덜 밝은 배경
-    primary: "#7269FF", // 브랜드 컬러
-    secondary: "#5049B3", // 브랜드 서브 컬러 (보색)
+    primary: "#7D79F2", // 브랜드 컬러
+    secondary: "#799BF2", // 브랜드 서브 컬러
     font1: "#242424", // 중요한 텍스트
     font2: "#6D6D6D", // 덜 중요한 텍스트
     border: "#D8D6E2", // 경계선 컬러
@@ -17,8 +17,8 @@ const themes = {
     bg: "#121212", // 가장 어두운 컬러
     bg2: "#272727", // 덜 어두운 컬러
     bg3: "#424242", // 더 덜 어두운 컬러
-    primary: "#5049B3", // 브랜드 컬러
-    secondary: "#3E388A", // 브랜드 서브 컬러
+    primary: "#7D79F2", // 브랜드 컬러
+    secondary: "#799BF2", // 브랜드 서브 컬러
     font1: "#DEDEDE", // 중요한 텍스트
     font2: "#999999", // 덜 중요한 텍스트
     border: "#373737", // 경계선 컬러
@@ -27,24 +27,29 @@ const themes = {
 
 const filters = {
   light: {
-    bg: "invert(86%) sepia(100%) saturate(0%) hue-rotate(42deg) brightness(106%) contrast(103%)", // 가장 밝은 배경
-    bg2: "invert(89%) sepia(2%) saturate(1308%) hue-rotate(204deg) brightness(108%) contrast(104%)", // 덜 밝은 배경
-    bg3: "invert(94%) sepia(50%) saturate(528%) hue-rotate(181deg) brightness(100%) contrast(97%)", // 더 덜 밝은 배경
-    primary: "invert(43%) sepia(15%) saturate(4177%) hue-rotate(215deg) brightness(101%) contrast(101%)", // 브랜드 컬러
-    secondary: "invert(15%) sepia(41%) saturate(885%) hue-rotate(208deg) brightness(93%) contrast(102%)", // 브랜드 서브 컬러
-    font1: "invert(0%) sepia(20%) saturate(53%) hue-rotate(169deg) brightness(78%) contrast(80%)", // 중요한 텍스트
-    font2: "invert(38%) sepia(0%) saturate(351%) hue-rotate(203deg) brightness(90%) contrast(89%)", // 덜 중요한 텍스트
-    border: "invert(92%) sepia(8%) saturate(223%) hue-rotate(210deg) brightness(98%) contrast(81%)", // 경계선 컬러
+    bg: "invert(100%) sepia(4%) saturate(5384%) hue-rotate(220deg) brightness(111%) contrast(97%)", // 가장 밝은 배경
+    bg2: "invert(100%) sepia(0%) saturate(1941%) hue-rotate(310deg) brightness(114%) contrast(98%)", // 덜 밝은 배경
+    bg3: "invert(98%) sepia(71%) saturate(211%) hue-rotate(175deg) brightness(110%) contrast(86%)", // 더 덜 밝은 배경
+    primary:
+      "invert(45%) sepia(79%) saturate(761%) hue-rotate(210deg) brightness(98%) contrast(94%)", // 브랜드 컬러
+    secondary:
+      "invert(74%) sepia(47%) saturate(6150%) hue-rotate(200deg) brightness(105%) contrast(90%)", // 브랜드 서브 컬러
+    font1: "invert(10%) sepia(4%) saturate(7%) hue-rotate(20deg) brightness(96%) contrast(89%)", // 중요한 텍스트
+    font2: "invert(45%) sepia(0%) saturate(1%) hue-rotate(303deg) brightness(91%) contrast(84%)", // 덜 중요한 텍스트
+    border:
+      "invert(98%) sepia(1%) saturate(6149%) hue-rotate(186deg) brightness(88%) contrast(100%)", // 경계선 컬러
   },
   dark: {
-    bg: "invert(5%) sepia(14%) saturate(6827%) hue-rotate(230deg) brightness(96%) contrast(100%)", // 가장 어두운 컬러
-    bg2: "invert(9%) sepia(6%) saturate(6713%) hue-rotate(224deg) brightness(95%) contrast(97%)", // 덜 어두운 컬러
-    bg3: "invert(13%) sepia(10%) saturate(3956%) hue-rotate(221deg) brightness(93%) contrast(89%)", // 더 덜 어두운 컬러
-    primary: "invert(43%) sepia(15%) saturate(4177%) hue-rotate(215deg) brightness(101%) contrast(101%)", // 브랜드 컬러
-    secondary: "invert(72%) sepia(8%) saturate(1882%) hue-rotate(207deg) brightness(96%) contrast(94%)", // 브랜드 서브 컬러
-    font1: "invert(99%) sepia(2%) saturate(251%) hue-rotate(234deg) brightness(114%) contrast(84%)", // 중요한 텍스트
-    font2: "invert(66%) sepia(12%) saturate(0%) hue-rotate(147deg) brightness(109%) contrast(84%)", // 덜 중요한 텍스트
-    border: "invert(19%) sepia(75%) saturate(619%) hue-rotate(209deg) brightness(93%) contrast(92%)", // 경계선 컬러
+    bg: "invert(7%) sepia(91%) saturate(19%) hue-rotate(318deg) brightness(95%) contrast(102%)", // 가장 어두운 컬러
+    bg2: "invert(10%) sepia(0%) saturate(4891%) hue-rotate(109deg) brightness(98%) contrast(92%)", // 덜 어두운 컬러
+    bg3: "invert(23%) sepia(4%) saturate(20%) hue-rotate(350deg) brightness(95%) contrast(88%)", // 더 덜 어두운 컬러
+    primary:
+      "invert(45%) sepia(79%) saturate(761%) hue-rotate(210deg) brightness(98%) contrast(94%)", // 브랜드 컬러
+    secondary:
+      "invert(74%) sepia(47%) saturate(6150%) hue-rotate(200deg) brightness(105%) contrast(90%)", // 브랜드 서브 컬러
+    font1: "invert(95%) sepia(3%) saturate(1%) hue-rotate(10deg) brightness(110%) contrast(74%)", // 중요한 텍스트
+    font2: " invert(63%) sepia(1%) saturate(0%) hue-rotate(209deg) brightness(97%) contrast(86%)", // 덜 중요한 텍스트
+    border: "invert(17%) sepia(0%) saturate(1%) hue-rotate(35deg) brightness(103%) contrast(89%)", // 경계선 컬러
   },
 };
 
